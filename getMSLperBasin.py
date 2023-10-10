1372fb7d226effbff8ee94dae28b7de456f8cfd6
nathaliedees <nathaliedees@hotmail.com>
2023-10-10 15:10:49 +0200

    Update getMSLperBasin.py

--- getMSLperBasin.py
+++ getMSLperBasin.py
@@ -1,3 +1,4 @@
+# %%
 import xarray as xr
 import os, regionmask
 import geopandas as gpd
@@ -9,26 +10,38 @@ shp = shp.to_crs(4326)
 
 files = [os.path.join('.', 'reanalysis', i) for i in os.listdir(r'.\reanalysis') if '2010' in i]
 dx = xr.open_mfdataset(r'.\reanalysis\*.nc')
-
+# %%
 # iterate over each basin
 for index, row in shp.iterrows():
+    if index == 1: 
+        # Get the polygon geometry
+        polygon = gpd.GeoSeries(row.geometry, crs='EPSG:4326')
 
-    # Get the polygon geometry
-    polygon = gpd.GeoSeries(row.geometry, crs='EPSG:4326')
+        regmask = regionmask.mask_geopandas(
+            polygon,
+            dx.station_y_coordinate,
+            dx.station_x_coordinate,
+            wrap_lon=180
+        )
+        basin = dx.where(~regmask.isnull(),drop=True)
+        basinAvg = basin.waterlevel.groupby('time.year').mean(dim='time').mean(dim='stations')
+        
+        df = basinAvg.to_dataframe().transpose().reset_index().drop('index', axis=1)
+        gdf = gpd.GeoDataFrame(row).transpose()
 
-    regmask = regionmask.mask_geopandas(
-        polygon,
-        dx.station_y_coordinate,
-        dx.station_x_coordinate,
-        wrap_lon=180
-    )
-    basin = dx.where(~regmask.isnull(),drop=True)
-    basinAvg = basin.waterlevel.groupby('time.year').mean(dim='time').mean(dim='stations')
+        basindf = gpd.GeoDataFrame(
+            pd.concat([gdf.reset_index(), df.reset_index()], axis=1))
+        basindf.crs = 'EPSG:4326'
+
+        if not os.path.exists(os.path.join('.', 'csv')):
+            os.makedirs(os.path.join('.', 'csv'))
+
+        name = row['Ecoregion'].replace(' ', '_')
+
+        df.transpose().to_csv(os.path.join('.', 'csv', name + '.csv'))
+
+        
+
+        
     
-    df = basinAvg.to_dataframe().transpose().reset_index().drop('index', axis=1)
-    gdf = gpd.GeoDataFrame(row).transpose()
-
-    basindf = gpd.GeoDataFrame(
-        pd.concat([gdf.reset_index(), df.reset_index()], axis=1))
-    basindf.crs = 'EPSG:4326'
-    
+# %%
